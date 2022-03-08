ce3d6d0a3da817609aafd79b5947869adc4c0ff8
petergeorgas <peter_georgas@yahoo.com>
2022-03-07 20:35:54 -0500

    Began adding connection to PostgreSQL instance

--- echo-sql/main.go
+++ echo-sql/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/keploy/go-sdk/integrations/kecho/v4"
 	"github.com/keploy/go-sdk/keploy"
 	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
 	"go.uber.org/zap"
 )
 
@@ -11,9 +15,34 @@ const port = "8080"
 
 var logger *zap.Logger
 
+const (
+	host     = "localhost"
+	db_port  = "5438"
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+)
+
 func main() {
 	logger, _ = zap.NewProduction()
 	defer logger.Sync() // flushes buffer
+
+	// Connect to PostgreSQL database
+	db_info := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, db_port, user, password, dbname)
+
+	db, err := sql.Open("postgres", db_info)
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal("Failed to open PostgreSQL instance:", zap.Error(err))
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal("Failed to establish connection to local PostgreSQL instance:", zap.Error(err))
+	}
 
 	// init Keploy
 	k := keploy.New(keploy.Config{
