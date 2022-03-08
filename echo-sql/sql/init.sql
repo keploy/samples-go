b875873f7fbdfcdf36dd0616508a4a2b312f790b
Peter Georgas <peter_georgas@yahoo.com>
2022-03-08 15:39:47 -0500

    Tweaked SQL init script

--- echo-sql/sql/init.sql
+++ echo-sql/sql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS url_map (
-    id varchar(8) NOT NULL,
-    redirect_url varchar(150) NOT NULL,
+    id char(8) NOT NULL,
+    redirect_url varchar(150) NOT NULL UNIQUE,
     created_at timestamp WITHOUT time zone NOT NULL,
     updated_at timestamp WITHOUT time zone NOT NULL,
     PRIMARY KEY(id)
