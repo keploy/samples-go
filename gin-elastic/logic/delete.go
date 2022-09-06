89c4856735feb91c36c7402707a8bcf6f6f3e9ff
iamskp99 <iamskp99@gmail.com>
2022-09-07 00:25:20 +0530

    fix: remove redundant code
    Signed-off-by: iamskp99 <iamskp99@gmail.com>

--- gin-elastic/logic/delete.go
+++ gin-elastic/logic/delete.go
@@ -10,37 +10,13 @@ import (
 )
 
 func DeleteMovieByDocumentID(rCtx context.Context, ctx context.Context, indexName string, docId string) {
-
-	// movies := ctx.Value(domain.MoviesKey).([]domain.Movie)
 	client := ctx.Value(domain.ClientKey).(*elasticsearch.Client)
 
 	rand.Seed(time.Now().UnixNano())
-	// documentID := rand.Intn(len(movies) - 1)
 	dreq := client.Delete.WithContext(rCtx)
 	response, err := client.Delete(indexName, docId, dreq)
 	if err != nil {
 		panic(err)
 	}
 	defer response.Body.Close()
-
-	// var getResponse = domain.GetResponse{}
-	// err = json.NewDecoder(response.Body).Decode(&getResponse)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// movieTitle := getResponse.Source.Title
-	// // fmt.Printf("✅ Movie with the ID %d: %s \n", documentID, movieTitle)
 }
-
-// func DeleteIndex(ctx context.Context, indexName string) {
-// 	client := ctx.Value(domain.ClientKey).(*elasticsearch.Client)
-
-// 	rand.Seed(time.Now().UnixNano())
-// 	// documentID := rand.Intn(len(movies) - 1)
-// 	// response, err := client.Bulk(ctx)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer response.Body.Close()
-// }
