50e6f37f50f739fd6dbdb58fafd3a87dd8cd6584
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2023-03-22 11:12:37 +0530

    db logging action changed
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- fasthttp-mysql/models/movies.go
+++ fasthttp-mysql/models/movies.go
@@ -3,24 +3,27 @@ package models
 import (
 	"database/sql"
 	"encoding/json"
+	"log"
 )
 
 func AddMovie(db *sql.DB, movie Movie) {
-	_, err := db.Exec("INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)", movie.Title, movie.Year, movie.Rating)
-	if err != nil {
-		panic(err.Error())
+	if _, err := db.Exec("INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)", movie.Title, movie.Year, movie.Rating); err != nil {
+		log.Fatal(err.Error())
 	}
 }
 
 func SingleMovie(db *sql.DB) (jsonMovie []byte) {
 	var movie Movie
+
 	if err := db.QueryRow("SELECT * FROM movies WHERE id = ?", countMovieID(db)).Scan(&movie.ID, &movie.Title, &movie.Year, &movie.Rating); err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	jsonMovie, err := json.Marshal(movie)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	return jsonMovie
 }
 
@@ -29,31 +32,34 @@ func AllMovies(db *sql.DB) (jsonMovies []byte) {
 		movie  Movie
 		movies []Movie
 	)
-	rows, err := db.Query("SELECT * FROM movies")
+
+	rows, err := db.Query("SELECT id, title, year, rating FROM movies")
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	for rows.Next() {
 		err := rows.Scan(&movie.ID, &movie.Title, &movie.Year, &movie.Rating)
 		if err != nil {
-			panic(err.Error())
+			log.Fatal(err.Error())
 		}
 		movies = append(movies, movie)
 	}
-	defer rows.Close()
+
 	jsonMovies, err = json.Marshal(movies)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err.Error())
 	}
+
 	return jsonMovies
 }
 
 func countMovieID(db *sql.DB) int {
-	var count int
-	if err := db.QueryRow("SELECT COUNT(ID) FROM movies").Scan(&count); err != nil {
-		panic(err.Error())
+	var movie Movie
+	if err := db.QueryRow("SELECT COUNT(ID) FROM movies;").Scan(&movie.ID); err != nil {
+		log.Fatal(err.Error())
 	}
-	return count
+	return movie.ID
 }
 
 // func AllMovieWithParameter(db *sql.DB, year, rating int) (jsonTitles []byte) {
@@ -64,19 +70,19 @@ func countMovieID(db *sql.DB) int {
 // 	yearRows, err := db.Query("SELECT * FROM movies WHERE year = ?", year)
 // 	ratingRow, err := db.Query("SELECT * FROM movies WHERE rating = ?", rating)
 // 	if err != nil {
-// 		panic(err.Error())
+// 		 log.Fatal(err.Error())
 // 	}
 // 	for rows.Next() {
 // 		err := rows.Scan(&movie.Title)
 // 		if err != nil {
-// 			panic(err.Error())
+// 			 log.Fatal(err.Error())
 // 		}
 // 		titles = append(titles, movie.Title)
 // 	}
 // 	defer rows.Close()
 // 	jsonTitles, err = json.Marshal(titles)
 // 	if err != nil {
-// 		panic(err.Error())
+// 		 log.Fatal(err.Error())
 // 	}
 
 // 	return jsonTitles
