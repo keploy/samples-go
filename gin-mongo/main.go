64fbea11555cdf1ce7dbf8a91ebc15ffc062dad6
gouravkrosx <gouravgreatkr@gmail.com>
2023-07-28 10:45:38 +0000

    refactor: remove go-sdk from gin-mongo sample app
    Signed-off-by: gouravkrosx <gouravgreatkr@gmail.com>

--- gin-mongo/main.go
+++ gin-mongo/main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"log"
-	"time"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 
-	// "github.com/keploy/go-sdk/integrations/kgin/v1"
-	// "github.com/keploy/go-sdk/integrations/kmongo"
-	// "github.com/keploy/go-sdk/keploy"
 	"go.uber.org/zap"
 )
 
-// var col *kmongo.Collection
 var col *mongo.Collection
-
 var logger *zap.Logger
 
 func main() {
@@ -24,41 +18,23 @@ func main() {
 
 	dbName, collection := "keploy", "url-shortener"
 
-	port := "8081"
-	// initialize keploy
-	// k := keploy.New(keploy.Config{
-	// 	App: keploy.AppConfig{
-	// 		Name: "sample-url-shortener",
-	// 		Port: port,
-	// 	},
-	// 	Server: keploy.ServerConfig{
-	// 		URL: "http://localhost:6789/api",
-	// 	},
-	// })
+	
+	client, err := New("localhost:27017", dbName)
+	if err != nil {
+		logger.Fatal("failed to create mgo db client", zap.Error(err))
+	}
+	db := client.Database(dbName)
+
+	col = db.Collection(collection)
+
+	port := "8080"
+
+	println("PID:",os.Getpid())
 
 	r := gin.Default()
-
-	// integrate keploy with gin router
-	// kgin.GinV1(k, r)
 
 	r.GET("/:param", getURL)
 	r.POST("/url", putURL)
 
-	r.GET("/", get)
-	go func() {
-		log.Printf("Please wait. dont make api call now for mongo deps")
-		time.Sleep(30 * time.Second)
-		log.Printf("you can make api call now for mongo deps")
-		client, err := New("localhost:27017", dbName)
-		if err != nil {
-			logger.Fatal("failed to create mgo db client", zap.Error(err))
-		}
-		db := client.Database(dbName)
-
-		// integrate keploy with mongo
-		// col = kmongo.NewCollection(db.Collection(collection))
-		col = db.Collection(collection)
-	}()
 	r.Run(":" + port)
-
 }
