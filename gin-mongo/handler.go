8e032a1ba7cbb63df35e3388120045e11bbe5c26
shivamsouravjha <2019145@iiitdmj.ac.in>
2023-08-23 07:13:14 +0000

    updating handler

--- gin-mongo/handler.go
+++ gin-mongo/handler.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"math/big"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -131,20 +132,114 @@ func getURL(c *gin.Context) {
 //Create a handler that makes a call to a wikipedia page and returns the response
 
 func getWiki(c *gin.Context) {
-	resp, err := http.Get("https://catfact.ninja/fact")
+	// Simulating a GET request
+	resp, err := http.Get("https://crudcrud.com/api/1f2d3fd3f4be42a3a3285208e7b8b7ad")
 	if err != nil {
 		log.Println("failed to make http call from handler. error: ", err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": `failed to make http call from handler. error: ` + err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to fetch data"})
+		return
 	}
-	//Send the response back to the client
+	fmt.Println(resp)
+	defer resp.Body.Close()
+
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println("failed to read http response. error: ", err.Error())
-		c.JSON(http.StatusBadRequest, gin.H{"error": `failed to read http response. error: ` + err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response"})
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{ //Send the response back to the client in JSON format with the status code 200
-		"ts":  time.Now().UnixNano(),
-		"url": string(respBody),
+
+	// Simulating a POST request
+	postResp, postErr := http.Post("https://reqres.in/api/users/2", "application/json", strings.NewReader(`{"data": "new data"}`))
+	if postErr != nil {
+		log.Println("failed to make http POST call. error: ", postErr.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create resource"})
+		return
+	}
+	fmt.Println(postResp.Body)
+	defer postResp.Body.Close()
+
+	// Simulating an UPDATE request
+	updateReq, updateErr := http.NewRequest("DELETE", "https://reqres.in/api/users/3", strings.NewReader(`{"data": "updated data"}`))
+	if updateErr != nil {
+		log.Println("failed to create PUT request. error: ", updateErr.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create update request"})
+		return
+	}
+	updateClient := http.Client{}
+	updateResp, updateErr := updateClient.Do(updateReq)
+	if updateErr != nil {
+		log.Println("failed to make http PUT call. error: ", updateErr.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to update resource"})
+		return
+	}
+	defer updateResp.Body.Close()
+
+	// Simulating a DELETE request
+	// deleteReq, deleteErr := http.NewRequest("DELETE", "https://reqres.in/api/users/2", nil)
+	// if deleteErr != nil {
+	// 	log.Println("failed to create DELETE request. error: ", deleteErr.Error())
+	// 	c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create delete request"})
+	// 	return
+	// }
+	// deleteClient := http.Client{}
+	// deleteResp, deleteErr := deleteClient.Do(deleteReq)
+	// if deleteErr != nil {
+	// 	log.Println("failed to make http DELETE call. error: ", deleteErr.Error())
+	// 	c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to delete resource"})
+	// 	return
+	// }
+	// defer deleteResp.Body.Close()
+
+	c.JSON(http.StatusOK, gin.H{
+		"ts":      time.Now().UnixNano(),
+		"url":     string(respBody),
+		"message": "Performed GET, POST, PUT, DELETE simulations",
+	})
+}
+
+func postWiki(c *gin.Context) {
+	var newFact map[string]interface{}
+	if err := c.BindJSON(&newFact); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// This is a stub. In real scenarios, you might make an HTTP POST request or save the data to a database.
+	log.Println("Received new fact: ", newFact)
+
+	c.JSON(http.StatusOK, gin.H{
+		"ts":      time.Now().UnixNano(),
+		"message": "Successfully created new fact.",
+	})
+}
+
+func updateWiki(c *gin.Context) {
+	var updatedFact map[string]interface{}
+	if err := c.BindJSON(&updatedFact); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// This is a stub. In real scenarios, you might make an HTTP PUT or PATCH request or update the data in a database.
+	log.Println("Updated fact: ", updatedFact)
+
+	c.JSON(http.StatusOK, gin.H{
+		"ts":      time.Now().UnixNano(),
+		"message": "Successfully updated fact.",
+	})
+}
+
+func deleteWiki(c *gin.Context) {
+	// This might represent an ID or some identifier for the fact you want to delete.
+	factID := c.Param("id")
+
+	// This is a stub. In real scenarios, you might make an HTTP DELETE request or delete the data from a database.
+	log.Println("Deleted fact with ID: ", factID)
+
+	c.JSON(http.StatusOK, gin.H{
+		"ts":      time.Now().UnixNano(),
+		"message": "Successfully deleted fact.",
 	})
 }
 
