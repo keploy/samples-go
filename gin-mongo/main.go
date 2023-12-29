33d5f8c4bee43f1a6ec80ee6c81b5c427b162c5e
Akash Kumar <meakash7902@gmail.com>
2023-12-29 16:40:27 +0530

    add graceful shutdown
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

--- gin-mongo/main.go
+++ gin-mongo/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -34,6 +39,27 @@ func main() {
 
 	r.GET("/:param", getURL)
 	r.POST("/url", putURL)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
 
-	r.Run(":" + port)
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		select {
+		case <-stopper:
+			ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+			defer cancel()
+			if err := srv.Shutdown(ctx); err != nil {
+				logger.Fatal("Server Shutdown:", zap.Error(err))
+			}
+			logger.Info("stopper called")
+		}
+	}()
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatal("listen: %s\n", zap.Error(err))
+	}
+	logger.Info("server exiting")
 }
