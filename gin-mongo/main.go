3dea411c17d41bbabaa4c16e3dfd523909dc3968
shivamsouravjha <shivamsouravjha@gmail.com>
2024-01-16 07:04:12 +0000

    check

--- gin-mongo/main.go
+++ gin-mongo/main.go
@@ -30,7 +30,7 @@ func main() {
 	}
 	db := client.Database(dbName)
 
-	col = db.Collection(collection)
+	col =db.Collection(collection)
 
 	port := "8080"
 
@@ -50,7 +50,7 @@ func main() {
 	go func() {
 		select {
 		case <-stopper:
-			ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			defer cancel()
 			if err := srv.Shutdown(ctx); err != nil {
 				logger.Fatal("Server Shutdown:", zap.Error(err))
