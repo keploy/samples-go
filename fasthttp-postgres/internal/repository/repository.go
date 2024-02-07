ac2142eed800a17dfa827500d24fc5d2a95e29e1
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2024-02-07 15:35:08 +0600

    feat: add simple books API with fasthttp & postgres (#51)
    * adds simple books api
    
    Signed-off-by: DarkhanShakhan <darkhan.shakhanov@gmail.com>
    
    * adds keploy testing
    
    Signed-off-by: DarkhanShakhan <darkhan.shakhanov@gmail.com>
    
    * Update app.go for v2
    
    * updated for v2
    
    Signed-off-by: Animesh pathak <kurosakiichigo.songoku@gmail.com>
    
    ---------
    
    Signed-off-by: DarkhanShakhan <darkhan.shakhanov@gmail.com>
    Signed-off-by: Animesh pathak <kurosakiichigo.songoku@gmail.com>
    Co-authored-by: Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
    Co-authored-by: Animesh pathak <kurosakiichigo.songoku@gmail.com>

---
+++ fasthttp-postgres/internal/repository/repository.go
@@ -0,0 +1,95 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fasthttp-postgres/internal/entity"
+	"fmt"
+)
+
+type Repository struct {
+	db *sql.DB
+}
+
+func NewRepository(db *sql.DB) *Repository {
+	return &Repository{
+		db: db,
+	}
+}
+
+func (r *Repository) GetAllAuthors(ctx context.Context) ([]entity.Author, error) {
+	var mm []entity.Author
+	rows, err := r.db.QueryContext(ctx, getAllAuthors)
+	if err != nil {
+		return nil, err
+	}
+	var m entity.Author
+	for rows.Next() {
+		if err = rows.Scan(&m.ID, &m.FirstName, &m.LastName); err != nil {
+			return nil, err
+		}
+		mm = append(mm, m)
+	}
+	return mm, nil
+}
+
+func (r *Repository) GetAllBooks(ctx context.Context) ([]entity.Book, error) {
+	var mm models
+	rows, err := r.db.QueryContext(ctx, getAllBooks)
+	if err != nil {
+		return nil, err
+	}
+	var m model
+	for rows.Next() {
+		if err = rows.Scan(&m.ID, &m.Title, &m.Year, &m.AuthorID, &m.FirstName, &m.LastName); err != nil {
+			return nil, err
+		}
+		mm = append(mm, m)
+	}
+	return mm.convert(), nil
+}
+
+func (r *Repository) GetBookById(ctx context.Context, id int) ([]entity.Book, error) {
+	var mm models
+	rows, err := r.db.QueryContext(ctx, getBookById, id)
+	if err != nil {
+		fmt.Println("1")
+		return nil, err
+	}
+	var m model
+	for rows.Next() {
+		if err = rows.Scan(&m.ID, &m.Title, &m.Year, &m.AuthorID, &m.FirstName, &m.LastName); err != nil {
+			fmt.Println("3")
+			return nil, err
+		}
+		mm = append(mm, m)
+	}
+	return mm.convert(), nil
+}
+
+func (r *Repository) GetBooksByAuthorId(ctx context.Context, id int) ([]entity.Book, error) {
+	var mm models
+	rows, err := r.db.QueryContext(ctx, getBooksByAuthorId, id)
+	if err != nil {
+		return nil, err
+	}
+	var m model
+	for rows.Next() {
+		if err = rows.Scan(&m.ID, &m.Title, &m.Year, &m.AuthorID, &m.FirstName, &m.LastName); err != nil {
+			return nil, err
+		}
+		mm = append(mm, m)
+	}
+	return mm.convert(), nil
+}
+
+func (r *Repository) CreateBook(ctx context.Context, b entity.Book) error {
+	m := convert(b)
+	_, err := r.db.ExecContext(ctx, createBookStmt, m.Title, m.Year, m.AuthorID)
+	return err
+}
+
+func (r *Repository) CreateAuthor(ctx context.Context, a entity.Author) error {
+	_, err := r.db.ExecContext(ctx, createAuthorStmt, a.FirstName, a.LastName)
+	return err
+}
