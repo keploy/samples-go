92a2aa7db84226996c48e82edb5072d9f6699980
dev-priyanshu15 <s.priyanshu.coder@gmail.com>
2025-04-16 09:10:29 +0530

    fix: formatted files for golangci-lint

--- gin-redis/helpers/redis/redisConnect.go
+++ gin-redis/helpers/redis/redisConnect.go
@@ -18,8 +18,8 @@ func Init() {
 	// Connect to the Redis server
 	RedisClient = redis.NewClient(&redis.Options{
 		Addr:     "redis:6379", // Redis server address
-		Password: "",               // No password for local Redis, set it if needed
-		DB:       0,                // Default DB
+		Password: "",           // No password for local Redis, set it if needed
+		DB:       0,            // Default DB
 	})
 	_, err := RedisClient.Ping().Result()
 	if err != nil {
